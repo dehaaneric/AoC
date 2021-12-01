9797bfef8e976ee52d191aca2d782844e2546212
Eric de Haan <eric.dehaan@socialschools.nl>
2021-12-01 11:28:41 +0100

    Add project files.

---
+++ Day1/Day1Task1.cs
@@ -0,0 +1,64 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Day1
+{
+    [MemoryDiagnoser]
+    public class Day1Task1
+    {
+        private int[] _measurements;
+
+        public Day1Task1()
+        {
+            string[] input = System.IO.File.ReadAllLines(@"day1input.txt");
+            _measurements = input.Select(int.Parse).ToArray();
+        }
+        /*
+            |     Method |      Mean |     Error |    StdDev | Allocated |
+            |----------- |----------:|----------:|----------:|----------:|
+            |   Generics | 28.428 us | 0.5538 us | 1.0670 us |      48 B |
+            | NoGenerics |  4.745 us | 0.0947 us | 0.1013 us |         - |
+        */
+
+        [Benchmark]
+        public int Generics()
+        {
+            int totalIncreases = 0;
+
+            int prevValue = _measurements.First();
+
+            foreach (var measurement in _measurements.Skip(1))
+            {
+                if (prevValue < measurement) totalIncreases++;
+
+                prevValue = measurement;
+            }
+
+            return totalIncreases;
+        }
+
+        [Benchmark]
+        public int NoGenerics()
+        {
+            int totalCount = _measurements.Length;
+            int totalIncreases = 0;
+            int prevValue = -1;
+
+            for (int index = 0; index < totalCount; index++)
+            {
+                var measurement = _measurements[index];
+                if(prevValue < 0)
+                {
+                    // Set first
+                    prevValue = measurement;
+                    continue;
+                }
+
+                if (prevValue < measurement) totalIncreases++;
+
+                prevValue = measurement;
+            }
+
+            return totalIncreases;
+        }
+    }
+}
---
+++ Day1/Day1Task2.cs
@@ -0,0 +1,96 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Day1
+{
+    [MemoryDiagnoser]
+    public class Day1Task2
+    {
+        int[] _measurements;
+        const int GroupingSize = 3;
+
+        public Day1Task2()
+        {
+            string[] input = System.IO.File.ReadAllLines(@"day1input.txt");
+            _measurements = input.Select(int.Parse).ToArray();
+        }
+
+        /*
+            |     Method |      Mean |    Error |    StdDev |   Gen 0 | Allocated |
+            |----------- |----------:|---------:|----------:|--------:|----------:|
+            |   Generics | 276.49 us | 5.498 us | 11.230 us | 63.4766 |    195 KB |
+            | NoGenerics |  14.95 us | 0.277 us |  0.432 us |  2.5635 |      8 KB |
+        */
+        [Benchmark]
+        public int Generics()
+        {
+            int totalCount = _measurements.Length;
+
+            var summedMeasurements = new List<int>(totalCount);
+            for (int index = 0; index < totalCount; index++)
+            {
+                var subList = _measurements.Skip(index).Take(3);
+
+                if (subList.Count() == 3)
+                    summedMeasurements.Add(subList.Sum());
+            }
+
+            int totalIncreases = 0;
+
+            int prevValue = _measurements.First();
+
+            foreach (var summedMeasurement in summedMeasurements.Skip(1))
+            {
+                if (prevValue < summedMeasurement) totalIncreases++;
+
+                prevValue = summedMeasurement;
+            }
+
+            return totalIncreases;
+        }
+
+        [Benchmark]
+        public int NoGenerics()
+        {
+            int totalCount = _measurements.Length;
+
+            int totalIncreases = 0;
+
+            var summedMeasurements = new List<int>(totalCount);
+            for (int index = 0; index < totalCount; index++)
+            {
+                if ((index + GroupingSize) > _measurements.Length) break;
+
+                var spannedMeasurements = 
+                    new Span<int>(_measurements, index, GroupingSize);
+
+                int numberOfItemsInSpan = spannedMeasurements.Length;
+                if (numberOfItemsInSpan != GroupingSize) break;
+
+                int sum = 0;
+                for (int spanIndex = 0; spanIndex < numberOfItemsInSpan; spanIndex++)
+                {
+                    sum += spannedMeasurements[spanIndex];
+                }
+                summedMeasurements.Add(sum);
+            }
+
+            int? prevValue = null;
+            var totalSummedCount = summedMeasurements.Count;
+            for (int index = 0; index < totalSummedCount; index++)
+            {
+                if (!prevValue.HasValue)
+                {
+                    // Set first
+                    prevValue = summedMeasurements[index];
+                    continue;
+                }
+
+                if (prevValue < summedMeasurements[index]) totalIncreases++;
+
+                prevValue = summedMeasurements[index];
+            }
+
+            return totalIncreases;
+        }
+    }
+}
