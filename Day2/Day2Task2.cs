e52122b5502f629f0151b686f5fed4b36a17141a
Eric de Haan <eric.dehaan@socialschools.nl>
2021-12-02 09:13:44 +0100

    Added two day-2 tasks

---
+++ Day2/Day2Task2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    public class Day2Task2
+    {
+        string[] _input;
+        public Day2Task2()
+        {
+            _input = System.IO.File.ReadAllLines(@"day2input.txt");
+        }
+
+        public int Calculate()
+        {
+            var linesCount = _input.Length;
+
+            var movements = new List<Movement>(linesCount);
+            foreach (var line in _input)
+            {
+                string[] parts = line.Split(' ');
+                movements.Add(new Movement(parts[0], int.Parse(parts[1])));
+            }
+
+            var location = Location.Start;
+            foreach (var movement in movements)
+            {
+                location = location.Move(movement);
+            }
+
+            int finalHorizontal = location.Position;
+            int finalDepth = location.Depth;
+            int total = finalDepth * finalHorizontal;
+
+            return total;
+        }
+
+        class Location
+        {
+            public Location(int position, int depth, int aim)
+            {
+                Position = position;
+                Depth = depth;
+                Aim = aim;
+            }
+
+            public int Position { get; }
+            public int Depth { get; }
+            public int Aim { get; }
+
+            public Location Move(Movement movement)
+            {
+                switch (movement.Action)
+                {
+                    case Movements.Forward:
+                        {
+                            return new Location(Position + movement.Value, Depth + (Aim * movement.Value), Aim);
+                        }
+                    case Movements.Up:
+                        {
+                            return new Location(Position, Depth, Aim - movement.Value);
+                        }
+                    case Movements.Down:
+                        {
+                            return new Location(Position, Depth, Aim + movement.Value);
+                        }
+                    default:
+                        throw new ArgumentException($"Not implemented action of {movement.Action}");
+                }
+            }
+
+            public static Location Start => new Location(0, 0, 0);
+        }
+
+        struct Movement
+        {
+            public Movement(string action, int value)
+            {
+                Action = action;
+                Value = value;
+            }
+            public string Action { get; }
+            public int Value { get; }
+        }
+
+        class Movements
+        {
+            public const string Forward = "forward";
+            public const string Up = "up";
+            public const string Down = "down";
+        }
+    }
+}
--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day2;
+
+var day2task1 = new Day2Task1();
+var total1 = day2task1.Calculate();
+
+Console.WriteLine($"Final score: {total1}"); // 2039912
+
+
+var day2task2 = new Day2Task2();
+var total2 = day2task2.Calculate();
+
+Console.WriteLine($"Final score: {total2}"); // 2039912
